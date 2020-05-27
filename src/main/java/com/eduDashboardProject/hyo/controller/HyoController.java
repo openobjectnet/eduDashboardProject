6e2d1d0515696bb8d12b56d3a258bbc069ee7de8
sajuni <sajuni11@naver.com>
2020-05-27 13:17:35 +0900

    hyo commit

---
+++ src/main/java/com/eduDashboardProject/hyo/controller/HyoController.java
@@ -0,0 +1,13 @@
+package com.eduDashboardProject.hyo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/sample")
+public class HyoController {
+
+	@Inject
+	
+	
+}
---
+++ src/main/java/com/eduDashboardProject/hyo/service/HyoService.java
@@ -0,0 +1,18 @@
+package com.eduDashboardProject.hyo.service;
+
+public interface HyoService {
+
+	
+	
+	/**
+	 * 공지사항 글 갯수 가져오기
+	 * @return
+	 * @throws Exception
+	 */
+	int getListCount()throws Exception;
+	
+	
+	
+	
+	
+}
