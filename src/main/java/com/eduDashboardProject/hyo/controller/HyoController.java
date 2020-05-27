0c1358660882aca21c7bfa650d2de86ab36fd6b6
sajuni <sajuni11@naver.com>
2020-05-27 18:29:37 +0900

    0527 커밋

--- src/main/java/com/eduDashboardProject/hyo/controller/HyoController.java
+++ src/main/java/com/eduDashboardProject/hyo/controller/HyoController.java
@@ -1,13 +1,80 @@
 package com.eduDashboardProject.hyo.controller;
 
+import java.util.ArrayList;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.eduDashboardProject.hyo.service.HyoService;
+import com.eduDashboardProject.hyo.vo.HyoboardVO;
+import com.eduDashboardProject.member.vo.MemberVO;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Controller
-@RequestMapping(value = "/sample")
+@Slf4j
+@RequestMapping("hyoboard/*")
 public class HyoController {
 
-	@Inject
+	@Resource(name="HyoService")
+	private HyoService hService;
+	
+	@RequestMapping(value = "/list", method = RequestMethod.GET)
+	public ModelAndView listGet(ModelAndView mv) throws Exception {
+		log.info("hyo_list.....");
+		
+		ArrayList<HyoboardVO> list = hService.selectList();
+		
+		mv.addObject("list",list);
+		mv.setViewName("hyoboard/list");
+		
+		return mv;
+	}
+	
+	@RequestMapping(value="/resisterView", method = RequestMethod.GET)
+	public ModelAndView boardresister(ModelAndView mv,HttpSession session) throws Exception {
+		
+		MemberVO m = (MemberVO)session.getAttribute("member");
+		
+		mv.addObject("m", m);
+		mv.setViewName("hyoboard/boardinsert");
+		
+		return mv;
+	}
+	
+	@RequestMapping(value="/insertBoard", method = RequestMethod.POST)
+	public String boardInsert(HyoboardVO hb,HttpSession session) throws Exception {
+		
+		MemberVO m = (MemberVO)session.getAttribute("member");
+		hb.setId(m.getId());
+		
+		int result = hService.insertBoard(hb);
+		
+		if(result > 0) {
+			return "redirect:/hyoboard/list";
+		}else {
+			return "ePage";
+		}
+		
+		
+	}
+	
+	@RequestMapping(value="/detailBoard", method = RequestMethod.GET)
+	public ModelAndView boardDetail(ModelAndView mv,int bno,HttpSession session) {
+		
+		HyoboardVO hb = hService.boardselect(bno);
+		
+		
+		return mv;
+		
+	}
+	
+	
 	
 	
 }
--- src/main/java/com/eduDashboardProject/hyo/service/HyoService.java
+++ src/main/java/com/eduDashboardProject/hyo/service/HyoService.java
@@ -1,17 +1,39 @@
 package com.eduDashboardProject.hyo.service;
 
-public interface HyoService {
+import java.util.ArrayList;
 
-	
+import com.eduDashboardProject.hyo.vo.HyoboardVO;
+
+public interface HyoService {
 	
 	/**
-	 * 공지사항 글 갯수 가져오기
+	 * 게시판 글 갯수 가져오기
 	 * @return
 	 * @throws Exception
 	 */
 	int getListCount()throws Exception;
 	
 	
+	/**
+	 * 게시판 리스트 불러오기
+	 * @return
+	 * @throws Exception
+	 */
+	ArrayList<HyoboardVO> selectList()throws Exception;
+
+
+	/**
+	 * 게시글 작성
+	 * @return
+	 */
+	int insertBoard(HyoboardVO hb);
+
+
+	/**
+	 * 게시글 1개 선택
+	 * @return
+	 */
+	HyoboardVO boardselect(int bno);
 	
 	
 	
