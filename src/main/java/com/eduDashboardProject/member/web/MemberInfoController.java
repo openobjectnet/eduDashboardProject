5c3d115062efcc8446493dffe346872f0b3e63a4
hodustar <hodustar@naver.com>
2020-06-01 10:24:31 +0900

    memberinfo 수정

--- src/main/java/com/eduDashboardProject/member/web/MemberInfoController.java
+++ src/main/java/com/eduDashboardProject/member/web/MemberInfoController.java
@@ -1,21 +1,9 @@
 package com.eduDashboardProject.member.web;
 
-import javax.annotation.Resource;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.eduDashboardProject.member.service.MemberInfoService;
-
-import lombok.extern.slf4j.Slf4j;
-
-@Controller
-@Slf4j
-@RequestMapping("memberinfo/*")
 public class MemberInfoController {
 
-	@Resource(name = "MemberInfoService")
-	private MemberInfoService memberinfoService;
+
 	
 	
 	
